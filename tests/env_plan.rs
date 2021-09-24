0e8f2784a3b1a16f309220dc95687e2cfe125d6b
Audun Halland <audun.halland@pm.me>
2021-09-24 23:53:50 +0200

    new 'env' strategy - single function for mount and patch

--- tests/env_plan.rs
+++ tests/env_plan.rs
@@ -4,81 +4,26 @@
 #[allow(unused_imports)]
 use hypp::prelude::*;
 
-/// NEW CODE:
-struct Env<H: ::hypp::Hypp> {
-    props: __ConditionalCallbackOwnedProps,
-    toggled: bool,
-    __phantom: std::marker::PhantomData<H>,
+enum PatchOrMount<'a, T> {
+    Patch(&'a mut T),
+    Mount(&'a mut Option<T>),
 }
 
-impl<H: ::hypp::Hypp + 'static> Env<H> {
-    // BUG: These won't work because of variable bindings
-    // in match patterns.
-    // But the idea with ::Init variants might still work,
-    // to reduce code duplication between mount and patch.
-    fn mount_span0_v0(
-        &self,
-        __cursor: &mut dyn ::hypp::Cursor<H>,
-        __binder: &mut dyn ::hypp::BindCallback<H, ConditionalCallback<H>>,
-    ) -> Result<__ConditionalCallbackSpan0<H>, ::hypp::Error> {
-        let toggled = self.toggled;
-
-        let __f2 = __cursor.const_exec_element(&__CONDITIONALCALLBACK_PRG0)?;
-        let __f1 = __cursor.attribute_value_callback().unwrap();
-        let __f3 = match toggled {
-            true => self.mount_span1_v0(__cursor)?,
-            false => self.mount_span1_v1(__cursor)?,
-        };
-        let __f6 = __cursor.const_exec_element(&__CONDITIONALCALLBACK_PRG3)?;
-
-        __binder.bind(
-            __f1.clone(),
-            ::hypp::ShimMethod::<ConditionalCallback<H>>(&|shim| {
-                shim.handle_click();
-            }),
-        );
-
-        Ok(__ConditionalCallbackSpan0::V0 {
-            __f1,
-            __f2,
-            __f3,
-            __f6,
-        })
-    }
-
-    fn mount_span0_v1(
-        &self,
-        __cursor: &mut dyn ::hypp::Cursor<H>,
-    ) -> Result<__ConditionalCallbackSpan0<H>, ::hypp::Error> {
-        Ok(__ConditionalCallbackSpan0::V1 {})
-    }
-
-    fn mount_span1_v0(
-        &self,
-        __cursor: &mut dyn ::hypp::Cursor<H>,
-    ) -> Result<__ConditionalCallbackSpan1<H>, ::hypp::Error> {
-        let __f4 = __cursor.const_exec_text(&__CONDITIONALCALLBACK_PRG1)?;
-        Ok(__ConditionalCallbackSpan1::V0 { __f4 })
-    }
-
-    fn mount_span1_v1(
-        &self,
-        __cursor: &mut dyn ::hypp::Cursor<H>,
-    ) -> Result<__ConditionalCallbackSpan1<H>, ::hypp::Error> {
-        let __f5 = __cursor.const_exec_text(&__CONDITIONALCALLBACK_PRG2)?;
-        Ok(__ConditionalCallbackSpan1::V1 { __f5 })
-    }
+/// NEW CODE: Env for storing props+state
+struct Env<H: ::hypp::Hypp> {
+    show_button: bool,
+    toggled: bool,
+    list: Vec<u32>,
+    __phantom: std::marker::PhantomData<H>,
 }
 
 // macro output:
 pub struct __ConditionalCallbackProps {
     pub show_button: bool,
 }
-pub struct __ConditionalCallbackOwnedProps {
-    pub show_button: bool,
-}
 pub struct __ConditionalCallbackShim<'c> {
-    props: &'c __ConditionalCallbackOwnedProps,
+    // just do this instead of exposing 'props' member:
+    show_button: &'c bool,
     toggled: ::hypp::state_ref::StateRef<'c, bool>,
 }
 impl<'c> __ConditionalCallbackShim<'c> {
@@ -96,6 +41,20 @@ static __CONDITIONALCALLBACK_PRG2: [::hypp::ConstOpCode; 1usize] =
     [::hypp::ConstOpCode::Text("Not toggled")];
 static __CONDITIONALCALLBACK_PRG3: [::hypp::ConstOpCode; 1usize] =
     [::hypp::ConstOpCode::ExitElement];
+
+struct __ConditionalCallbackSpanRoot<H: ::hypp::Hypp> {
+    __f0: __ConditionalCallbackSpan0<H>,
+}
+
+impl<H: ::hypp::Hypp + 'static> ::hypp::Span<H> for __ConditionalCallbackSpanRoot<H> {
+    fn is_anchored(&self) -> bool {
+        unimplemented!()
+    }
+    fn pass(&mut self, __cursor: &mut dyn ::hypp::Cursor<H>, op: ::hypp::SpanOp) -> bool {
+        ::hypp::span::pass(&mut [&mut self.__f0], __cursor, op)
+    }
+}
+
 enum __ConditionalCallbackSpan0<H: ::hypp::Hypp> {
     Init,
     V0 {
@@ -173,7 +132,7 @@ impl<H: ::hypp::Hypp + 'static> ::hypp::Span<H> for __ConditionalCallbackSpan1<H
 #[allow(dead_code)]
 pub struct ConditionalCallback<H: ::hypp::Hypp> {
     env: Env<H>,
-    __f0: __ConditionalCallbackSpan0<H>,
+    span: __ConditionalCallbackSpanRoot<H>,
     __anchor: H::Anchor,
     __weak_self: Option<::std::rc::Weak<::std::cell::RefCell<Self>>>,
 }
@@ -188,18 +147,25 @@ impl<H: ::hypp::Hypp + 'static> ConditionalCallback<H> {
         let mut __binder: ::hypp::shim::LazyBinder<H, Self> = ::hypp::shim::LazyBinder::new();
 
         let env = Env {
-            props: __ConditionalCallbackOwnedProps { show_button },
+            show_button,
             toggled: Default::default(),
+            list: vec![],
             __phantom: std::marker::PhantomData,
         };
 
-        let mut __f0 = __ConditionalCallbackSpan0::Init;
-        Self::patch_f0(&mut __f0, &env, &__updates, __cursor, &mut __binder);
+        let mut span = None;
+        Self::patch_or_mount(
+            PatchOrMount::Mount(&mut span),
+            &env,
+            &__updates,
+            __cursor,
+            &mut __binder,
+        );
 
         let __weak_self = None;
         let __mounted = ::std::rc::Rc::new(::std::cell::RefCell::new(ConditionalCallback {
             env,
-            __f0,
+            span: span.unwrap(),
             __anchor,
             __weak_self,
         }));
@@ -209,78 +175,146 @@ impl<H: ::hypp::Hypp + 'static> ConditionalCallback<H> {
         Ok(::hypp::handle::Shared::new(__mounted))
     }
 
-    fn patch_root(
-        &mut self,
-        __updates: &[bool],
-        __cursor: &mut dyn ::hypp::Cursor<H>,
-        __binder: &mut dyn ::hypp::BindCallback<H, ConditionalCallback<H>>,
-    ) {
-        Self::patch_f0(&mut self.__f0, &self.env, __updates, __cursor, __binder);
-    }
-
     // Patch only one root conditional.
     // can be used by initial mount.
-    fn patch_f0(
-        mut __f0: &mut __ConditionalCallbackSpan0<H>,
+    fn patch_or_mount(
+        __root: PatchOrMount<__ConditionalCallbackSpanRoot<H>>,
         __env: &Env<H>,
         __updates: &[bool],
         __cursor: &mut dyn ::hypp::Cursor<H>,
-        __binder: &mut dyn ::hypp::BindCallback<H, ConditionalCallback<H>>,
-    ) {
-        let show_button = __env.props.show_button;
+        __bind: &mut dyn ::hypp::BindCallback<H, ConditionalCallback<H>>,
+    ) -> Result<(), ::hypp::Error> {
+        let show_button = __env.show_button;
         let toggled = __env.toggled;
+        let list = &__env.list;
 
-        match (&mut __f0, show_button) {
-            (
-                __ConditionalCallbackSpan0::V0 {
-                    ref __f1,
-                    ref __f2,
-                    ref mut __f3,
-                    ref __f6,
-                    ..
-                },
-                true,
-            ) => {
-                __cursor.move_to_children_of(&__f2);
-                match (&__f3, toggled) {
-                    (__ConditionalCallbackSpan1::V0 { ref __f4, .. }, true) => {
-                        // Can do this
-                        __f3.pass_over(__cursor);
-                        // Instead of this:
-                        // __cursor.move_to_following_sibling_of(__f4.as_node());
-                    }
-                    (
-                        __ConditionalCallbackSpan1::Init | __ConditionalCallbackSpan1::V1 { .. },
-                        true,
-                    ) => {
-                        __f3.erase(__cursor);
-                        *__f3 = __env.mount_span1_v0(__cursor).unwrap();
-                    }
-                    (__ConditionalCallbackSpan1::V1 { ref __f5, .. }, false) => {
-                        __f3.pass_over(__cursor);
-                    }
-                    (
-                        __ConditionalCallbackSpan1::Init | __ConditionalCallbackSpan1::V0 { .. },
-                        false,
-                    ) => {
-                        __f3.erase(__cursor);
-                        *__f3 = __env.mount_span1_v1(__cursor).unwrap();
+        let mut do_with_list = |__cursor: &mut dyn ::hypp::Cursor<H>| -> Result<(), ::hypp::Error> {
+            for item in list.iter() {}
+            Ok(())
+        };
+
+        let mut do_with_list2 =
+            |__cursor: &mut dyn ::hypp::Cursor<H>| -> Result<(), ::hypp::Error> {
+                for item in list.iter() {}
+                Ok(())
+            };
+
+        let mut patch_span0 = |mut __f0: &mut __ConditionalCallbackSpan0<H>,
+                               __cursor: &mut dyn ::hypp::Cursor<H>|
+         -> Result<(), ::hypp::Error> {
+            match show_button {
+                // Single pattern here!
+                true => {
+                    let mount_span1_v0 = |__cursor: &mut dyn ::hypp::Cursor<H>| -> Result<
+                        __ConditionalCallbackSpan1<H>,
+                        ::hypp::Error,
+                    > {
+                        let __f4 = __cursor.const_exec_text(&__CONDITIONALCALLBACK_PRG1)?;
+                        Ok(__ConditionalCallbackSpan1::V0 { __f4 })
+                    };
+
+                    let mount_span1_v1 = |__cursor: &mut dyn ::hypp::Cursor<H>| -> Result<
+                        __ConditionalCallbackSpan1<H>,
+                        ::hypp::Error,
+                    > {
+                        let __f5 = __cursor.const_exec_text(&__CONDITIONALCALLBACK_PRG2)?;
+                        Ok(__ConditionalCallbackSpan1::V1 { __f5 })
+                    };
+
+                    match &mut __f0 {
+                        __ConditionalCallbackSpan0::V0 {
+                            ref __f1,
+                            ref __f2,
+                            ref mut __f3,
+                            ref __f6,
+                            ..
+                        } => {
+                            // Patch internals of V0
+                            __cursor.move_to_children_of(&__f2);
+                            match toggled {
+                                true => {
+                                    match &__f3 {
+                                        __ConditionalCallbackSpan1::V0 { ref __f4, .. } => {
+                                            // Can do this
+                                            __f3.pass_over(__cursor);
+                                            // Instead of this:
+                                            // __cursor.move_to_following_sibling_of(__f4.as_node());
+                                        }
+                                        __ConditionalCallbackSpan1::Init
+                                        | __ConditionalCallbackSpan1::V1 { .. } => {
+                                            __f3.erase(__cursor);
+                                            *__f3 = mount_span1_v0(__cursor)?;
+                                        }
+                                    }
+                                }
+                                false => match &__f3 {
+                                    __ConditionalCallbackSpan1::V1 { ref __f5, .. } => {
+                                        __f3.pass_over(__cursor);
+                                    }
+                                    __ConditionalCallbackSpan1::Init
+                                    | __ConditionalCallbackSpan1::V0 { .. } => {
+                                        __f3.erase(__cursor);
+                                        *__f3 = mount_span1_v1(__cursor)?;
+                                    }
+                                },
+                            }
+                            // Just pass the nodes instead? We know statically what they will be
+                            __cursor.move_to_following_sibling_of(__f6.as_node());
+                        }
+                        __ConditionalCallbackSpan0::Init
+                        | __ConditionalCallbackSpan0::V1 { .. } => {
+                            __f0.erase(__cursor);
+
+                            let __f2 = __cursor.const_exec_element(&__CONDITIONALCALLBACK_PRG0)?;
+                            let __f1 = __cursor.attribute_value_callback().unwrap();
+                            let __f3 = match toggled {
+                                true => mount_span1_v0(__cursor)?,
+                                false => mount_span1_v1(__cursor)?,
+                            };
+                            let __f6 = __cursor.const_exec_element(&__CONDITIONALCALLBACK_PRG3)?;
+
+                            __bind.bind(
+                                __f1.clone(),
+                                ::hypp::ShimMethod::<ConditionalCallback<H>>(&|shim| {
+                                    shim.handle_click();
+                                }),
+                            );
+
+                            *__f0 = __ConditionalCallbackSpan0::V0 {
+                                __f1,
+                                __f2,
+                                __f3,
+                                __f6,
+                            };
+                        }
                     }
                 }
-                // Just pass the nodes instead.. We know statically what they will be
-                __cursor.move_to_following_sibling_of(__f6.as_node());
+                false => match &mut __f0 {
+                    __ConditionalCallbackSpan0::V1 { .. } => {}
+                    __ConditionalCallbackSpan0::Init | __ConditionalCallbackSpan0::V0 { .. } => {
+                        __f0.erase(__cursor);
+                        let __mounted = __ConditionalCallbackSpan0::V1 {};
+                        *__f0 = __mounted;
+                    }
+                },
             }
-            (__ConditionalCallbackSpan0::Init | __ConditionalCallbackSpan0::V1 { .. }, true) => {
-                __f0.erase(__cursor);
-                *__f0 = __env.mount_span0_v0(__cursor, __binder).unwrap();
+            Ok(())
+        };
+
+        match __root {
+            PatchOrMount::Mount(span) => {
+                let mut __f0 = __ConditionalCallbackSpan0::Init;
+                do_with_list(__cursor);
+                do_with_list2(__cursor);
+                patch_span0(&mut __f0, __cursor)?;
+                *span = Some(__ConditionalCallbackSpanRoot { __f0 });
             }
-            (__ConditionalCallbackSpan0::V1 { .. }, false) => {}
-            (__ConditionalCallbackSpan0::Init | __ConditionalCallbackSpan0::V0 { .. }, false) => {
-                __f0.erase(__cursor);
-                let __mounted = __ConditionalCallbackSpan0::V1 {};
-                *__f0 = __mounted;
+            PatchOrMount::Patch(span) => {
+                patch_span0(&mut span.__f0, __cursor)?;
             }
         }
+
+        Ok(())
     }
 }
 impl<H: ::hypp::Hypp> ::hypp::handle::ToHandle for ConditionalCallback<H> {
@@ -291,7 +325,7 @@ impl<H: ::hypp::Hypp + 'static> ::hypp::Span<H> for ConditionalCallback<H> {
         unimplemented!()
     }
     fn pass(&mut self, __cursor: &mut dyn ::hypp::Cursor<H>, op: ::hypp::SpanOp) -> bool {
-        ::hypp::span::pass(&mut [&mut self.__f0], __cursor, op)
+        self.span.pass(__cursor, op)
     }
     fn pass_over(&mut self, __cursor: &mut dyn ::hypp::Cursor<H>) -> bool {
         self.__anchor = __cursor.anchor();
@@ -310,13 +344,19 @@ impl<'p, H: ::hypp::Hypp + 'static> ::hypp::Component<'p, H> for ConditionalCall
         __cursor: &mut dyn ::hypp::Cursor<H>,
     ) {
         let mut __updates: [bool; 2usize] = [false; 2usize];
-        if self.env.props.show_button != show_button {
-            self.env.props.show_button = show_button;
+        if self.env.show_button != show_button {
+            self.env.show_button = show_button;
             __updates[0usize] = true;
         }
 
         let mut binder = ::hypp::shim::Binder::from_opt_weak(&self.__weak_self);
-        self.patch_root(&__updates, __cursor, &mut binder);
+        Self::patch_or_mount(
+            PatchOrMount::Patch(&mut self.span),
+            &self.env,
+            &__updates,
+            __cursor,
+            &mut binder,
+        );
     }
 }
 impl<'p, H: ::hypp::Hypp + 'static> ::hypp::ShimTrampoline for ConditionalCallback<H> {
@@ -324,7 +364,7 @@ impl<'p, H: ::hypp::Hypp + 'static> ::hypp::ShimTrampoline for ConditionalCallba
     fn shim_trampoline(&mut self, method: ::hypp::ShimMethod<Self>) {
         let mut __updates: [bool; 2usize] = [false; 2usize];
         let mut shim = __ConditionalCallbackShim {
-            props: &self.env.props,
+            show_button: &self.env.show_button,
             toggled: ::hypp::state_ref::StateRef::new(
                 &mut self.env.toggled,
                 &mut __updates[1usize],
@@ -334,7 +374,13 @@ impl<'p, H: ::hypp::Hypp + 'static> ::hypp::ShimTrampoline for ConditionalCallba
 
         let mut cursor = self.__anchor.create_builder();
         let mut binder = ::hypp::shim::Binder::from_opt_weak(&self.__weak_self);
-        self.patch_root(&__updates, &mut cursor, &mut binder);
+        Self::patch_or_mount(
+            PatchOrMount::Patch(&mut self.span),
+            &self.env,
+            &__updates,
+            &mut cursor,
+            &mut binder,
+        );
     }
 }
 
